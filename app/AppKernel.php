f9c8ca1ae2cac68b4300615815b3645c7993cfc0
Vincent Chalnot <vchalnot@clever-age.com>
2019-04-23 15:01:16 +0200

    Testing additional configs with API Platform and fixing docker

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,7 @@ class AppKernel extends Kernel
                 new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
                 new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
                 new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+                new Sidus\DoctrineDebugBundle\SidusDoctrineDebugBundle(),
             ];
         }
 
